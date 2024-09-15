598bd63423291ea723133ee2700d812241f60c3b
dmpdump <dmpdump@tuta.io>
2024-09-15 17:08:40 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,4 @@
-/*
- * Disclaimer:
- * This code was created for educational purposes only.
- * The author does not take responsibility for any misuse 
- * or unintended consequences arising from its application.
- * Users are encouraged to exercise caution and adhere to 
- * all relevant laws and regulations when utilizing this code.
- */
+// Disclaimer: This code was created for educational purposes only. The author does not take responsibility for any misuse or unintended consequences arising from its application. Users are encouraged to exercise caution and adhere to all relevant laws and regulations when utilizing this code.
 
 #include <Windows.h>
 #include <wincrypt.h>
